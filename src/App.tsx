22c42439c3af0fec997af14db72112dda10a2964
beau-haldane <beauhaldane@gmail.com>
2024-02-22 00:58:03 +1000

    UI improvements

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import * as THREE from "three";
 import { wrap, proxy } from "comlink";
 import PresentationViewer from "./replicad-studio-components/PresentationViewer.jsx";
 import { ParameterSelector } from "./components/ParameterSelector.tsx";
+import { DrumShell, Lugs } from "./model/types.ts";
 
 // @ts-expect-error - Property 'DefaultUp' does not exist on type 'typeof Object3D', however this code executes fine and is necessary to the correct rotational axis
 THREE.Object3D.DefaultUp.set(0, 0, 1);
@@ -19,16 +20,27 @@ function convertToPercentage(number) {
 export default function ReplicadApp() {
   const [model, setModel] = useState(null);
   const [modelProgress, setModelProgress] = useState("0%");
-  const [shellParameters, setShellParameters] = useState({
-    lugs: 8,
-    diameterInches: 12,
-    depthInches: 8,
-    shellThickness: 9,
-    lugHoleDiameter: 5,
-    lugHoleSpacing: 65,
-    fitmentTolerance: 0.2,
+  const [shell, setShell] = useState<DrumShell>({
+    diameterInches: 14,
+    depthInches: 6.5,
+    shellThickness: 12,
   });
-  
+  const [lugs, setLugs] = useState<Lugs>({
+    lugType: 'doublePoint',
+    lugRows: 2,
+    lugNumber: 8,
+    lugHoleSpacing: 20,
+    lugHoleDiameter: 5,
+    lugHolePocketDiameter: 8,
+    lugHolePocketDepth: 2,
+    lugHoleDistanceFromEdge: 45
+  })
+  const [printableDrum, setPrintableDrum] = useState({
+    fitmentTolerance: 0.2,
+    shell,
+    lugs,
+  });
+
   const updateModelProgress = (number) => {
     setModelProgress(convertToPercentage(number));
   };
@@ -40,26 +52,33 @@ export default function ReplicadApp() {
     setModelProgress("0%");
     // @ts-expect-error - see TODO on line 1
     cad.createAssembly(
-      shellParameters,
+      printableDrum,
       proxy(updateModelProgress),
       proxy(updateModel)
     );
   };
 
-  // const downloadModel = async () => {
-  //   const blob = await cad.createBlob(shellParameters);
-  //   FileSaver.saveAs(blob, "drum.stl");
-  // };
-
   useEffect(() => {
     generateModel();
   }, []);
 
+  useEffect(() => {
+    setPrintableDrum({
+      lugs,
+      shell,
+      fitmentTolerance: 0.2,
+    })
+  }, [shell, lugs])
+
   return (
     <>
       <ParameterSelector
-        shellParameters={shellParameters}
-        setShellParameters={setShellParameters}
+        printableDrum={printableDrum}
+        setPrintableDrum={setPrintableDrum}
+        shell={shell}
+        setShell={setShell}
+        lugs={lugs}
+        setLugs={setLugs}
         generateModel={generateModel}
       />
       <section style={{ height: "100vh" }}>
--- src/components/ParameterSelector.tsx
+++ src/components/ParameterSelector.tsx
@@ -1,76 +1,35 @@
-import { NumberInput } from "@mui/base/Unstable_NumberInput/NumberInput";
-import { Button, FormLabel, Input, TextField } from "@mui/material";
 import React from "react";
-import { CustomNumberInput } from "./CustomNumberInput";
+import { Button } from "@mui/material";
+import { NumberInput } from "./CustomNumberInput";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import SelectInput from "./SelectInput";
 
 export const ParameterSelector = ({
-  shellParameters,
-  setShellParameters,
+  printableDrum,
+  setPrintableDrum,
+  shell,
+  setShell,
+  lugs,
+  setLugs,
   generateModel,
 }) => {
-  // const [shellParameters, setShellParameters] = useState({
-  //   lugs: 8,
-  //   diameterInches: 14,
-  //   depthInches: 6.5,
-  //   shellThickness: 9,
-  //   lugHoleDiameter: 5,
-  //   lugHoleSpacing: 50,
-  //   fitmentTolerance: 0.2,
-  // });
-
-  const updateShellDimensions = ({
-    newDiameterInches,
-    newDepthInches,
-    newShellThickness,
-    newLugs,
-    newLugHoleDiameter,
-    newLugHoleSpacing,
-    newFitmentTolerance,
-  }: Partial<{
-    newDiameterInches: number;
-    newDepthInches: number;
-    newShellThickness: number;
-    newLugs: number;
-    newLugHoleDiameter: number;
-    newLugHoleSpacing: number;
-    newFitmentTolerance: number;
-  }>) => {
-    setShellParameters({
-      diameterInches: newDiameterInches ?? shellParameters.diameterInches,
-      depthInches: newDepthInches ?? shellParameters.depthInches,
-      shellThickness: newShellThickness ?? shellParameters.shellThickness,
-      lugs: newLugs ?? shellParameters.lugs,
-      lugHoleDiameter: newLugHoleDiameter ?? shellParameters.lugHoleDiameter,
-      lugHoleSpacing: newLugHoleSpacing ?? shellParameters.lugHoleSpacing,
-      fitmentTolerance: newFitmentTolerance ?? shellParameters.fitmentTolerance,
-    });
+  const updateState = (state, setState, property, propertyName) => {
+    const newState = { ...state };
+    newState[propertyName] = property;
+    setState(newState);
   };
-
-  const NumInput = ({ label, defaultValue, min, max, step, propertyName }) => {
-    return (
-      <div>
-        <FormLabel sx={{fontSize: '14px',}}>{label}</FormLabel>
-        <CustomNumberInput
-          aria-label={label}
-          value={defaultValue}
-          min={min}
-          max={max}
-          step={step}
-          onChange={(event, val) =>
-            updateShellDimensions({ [propertyName]: val })
-          }
-        />
-      </div>
-    );
-  };
-
   const inputRef = React.useRef<HTMLInputElement | null>(null);
 
   return (
     <div
       style={{
         display: "flex",
+        fontFamily: "Roboto",
         flexDirection: "column",
+        width: "300px",
         gap: "10px",
         padding: "10px",
         position: "absolute",
@@ -80,66 +39,167 @@ export const ParameterSelector = ({
         backgroundColor: "#FFF",
       }}
     >
-      <NumInput
-        label="Diameter (Inches)"
-        defaultValue={shellParameters.diameterInches}
-        min={8}
-        max={16}
-        step={1}
-        propertyName="newDiameterInches"
-      />
-      <NumInput
-        label="Depth (inches)"
-        defaultValue={shellParameters.depthInches}
-        min={4}
-        max={16}
-        step={0.25}
-        propertyName="newDepthInches"
-      />
-      <NumInput
-        label="Lugs"
-        defaultValue={shellParameters.lugs}
-        min={6}
-        max={12}
-        step={1}
-        propertyName="newLugs"
-      />
-      <NumInput
-        label="Lug Hole Diameter"
-        defaultValue={shellParameters.lugHoleDiameter}
-        min={1}
-        max={8}
-        step={0.1}
-        propertyName="newLugHoleDiameter"
-      />
-      <NumInput
-        label="Lug Hole Spacing"
-        defaultValue={shellParameters.lugHoleSpacing}
-        min={0}
-        max={
-          shellParameters.depthInches * 25.4 -
-          60 -
-          shellParameters.lugHoleDiameter * 2
-        }
-        step={1}
-        propertyName="newLugHoleSpacing"
-      />
-      <NumInput
-        label="Shell Thickness"
-        defaultValue={shellParameters.shellThickness}
-        min={6}
-        max={(shellParameters.diameterInches * 25.4) / 3}
-        step={1}
-        propertyName="newShellThickness"
-      />
-      <NumInput
-        label="Fitment Tolerance"
-        defaultValue={shellParameters.fitmentTolerance}
-        min={0}
-        max={1}
-        step={0.05}
-        propertyName="newFitmentTolerance"
-      />
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <strong>Drum Shell</strong>
+        </AccordionSummary>
+        <AccordionDetails>
+          <NumberInput
+            label="Fitment Tolerance"
+            defaultValue={printableDrum.fitmentTolerance}
+            min={0}
+            max={1}
+            step={0.05}
+            propertyName="fitmentTolerance"
+            updateState={updateState}
+            state={printableDrum}
+            setState={setPrintableDrum}
+          />
+          <NumberInput
+            label="Diameter (Inches)"
+            defaultValue={shell.diameterInches}
+            min={8}
+            max={16}
+            step={1}
+            propertyName="diameterInches"
+            updateState={updateState}
+            state={shell}
+            setState={setShell}
+          />
+          <NumberInput
+            label="Depth (inches)"
+            defaultValue={shell.depthInches}
+            min={4}
+            max={16}
+            step={0.25}
+            propertyName="depthInches"
+            updateState={updateState}
+            state={shell}
+            setState={setShell}
+          />
+          <NumberInput
+            label="Shell Thickness"
+            defaultValue={shell.shellThickness}
+            min={6}
+            max={(shell.diameterInches * 25.4) / 3}
+            step={1}
+            propertyName="shellThickness"
+            updateState={updateState}
+            state={shell}
+            setState={setShell}
+          />
+        </AccordionDetails>
+      </Accordion>
+      <Accordion sx={{ width: "100%" }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <strong>Lugs</strong>
+        </AccordionSummary>
+        <AccordionDetails>
+          <SelectInput
+            label="Lug Type"
+            defaultValue={lugs.lugType}
+            values={[
+              { value: "singlePoint", displayText: "Single Point" },
+              { value: "doublePoint", displayText: "Double Point" },
+            ]}
+            propertyName="lugType"
+            updateState={updateState}
+            state={lugs}
+            setState={setLugs}
+          />
+          <NumberInput
+            label="Lugs"
+            defaultValue={lugs.lugNumber}
+            min={6}
+            max={12}
+            step={1}
+            propertyName="lugNumber"
+            updateState={updateState}
+            state={lugs}
+            setState={setLugs}
+          />
+          <SelectInput
+            label="Lug Rows"
+            defaultValue={lugs.lugRows}
+            values={[
+              { value: 1, displayText: 1 },
+              { value: 2, displayText: 2 },
+            ]}
+            propertyName="lugRows"
+            updateState={updateState}
+            state={lugs}
+            setState={setLugs}
+          />
+          {lugs.lugType === "doublePoint" && (
+            <NumberInput
+              label="Lug Hole Spacing"
+              defaultValue={lugs.lugHoleSpacing}
+              min={0}
+              max={shell.depthInches * 25.4 - 60 - lugs.lugHoleDiameter * 2}
+              step={1}
+              propertyName="lugHoleSpacing"
+              updateState={updateState}
+              state={lugs}
+              setState={setLugs}
+            />
+          )}
+          {lugs.lugRows === 2 && (
+            <NumberInput
+              label="Lug Distance From Edge"
+              defaultValue={lugs.lugHoleDistanceFromEdge}
+              min={0}
+              max={((shell.depthInches * 25.4) - (60)) / 2}
+              step={1}
+              propertyName="lugHoleDistanceFromEdge"
+              updateState={updateState}
+              state={lugs}
+              setState={setLugs}
+            />
+          )}
+          <NumberInput
+            label="Lug Hole Diameter"
+            defaultValue={lugs.lugHoleDiameter}
+            min={1}
+            max={8}
+            step={0.1}
+            propertyName="lugHoleDiameter"
+            updateState={updateState}
+            state={lugs}
+            setState={setLugs}
+          />
+          <NumberInput
+            label="Lug Hole Pocket Diameter"
+            defaultValue={lugs.lugHolePocketDiameter}
+            min={1}
+            max={12}
+            step={0.1}
+            propertyName="lugHolePocketDiameter"
+            updateState={updateState}
+            state={lugs}
+            setState={setLugs}
+          />
+          <NumberInput
+            label="Lug Hole Pocket Depth"
+            defaultValue={lugs.lugHolePocketDepth}
+            min={0}
+            max={shell.shellThickness}
+            step={0.1}
+            propertyName="lugHolePocketDepth"
+            updateState={updateState}
+            state={lugs}
+            setState={setLugs}
+          />
+        </AccordionDetails>
+      </Accordion>
+
       <Button variant="contained" onClick={() => generateModel()}>
         Generate
       </Button>
