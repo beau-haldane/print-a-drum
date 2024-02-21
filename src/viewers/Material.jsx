492746767ca7edf8e4254287fe8fc56de5966b0b
beau-haldane <beauhaldane@gmail.com>
2024-02-21 15:57:36 +1000

    rewrite project

--- src/viewers/Material.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useTexture } from "@react-three/drei";
-
-export default function Material(props) {
-  const [matcap1] = useTexture(["/textures/matcap-1.png"]);
-  return <meshMatcapMaterial color="#5a8296" matcap={matcap1} {...props} />;
-}
---
+++ src/worker.js
@@ -0,0 +1,109 @@
+import opencascade from "replicad-opencascadejs/src/replicad_single.js";
+import opencascadeWasm from "replicad-opencascadejs/src/replicad_single.wasm?url";
+import { setOC } from "replicad";
+import { expose } from "comlink";
+import { generate3DPSD } from "./model/3dpsd";
+import * as replicad from "replicad";
+
+let loaded = false;
+const init = async () => {
+  if (loaded) return Promise.resolve(true);
+
+  const OC = await opencascade({
+    locateFile: () => opencascadeWasm,
+  });
+
+  loaded = true;
+  setOC(OC);
+
+  return true;
+};
+
+const started = init();
+
+const isBlueprintLike = (shape) => {
+  return (
+    shape instanceof replicad.Blueprint ||
+    shape instanceof replicad.Blueprints ||
+    shape instanceof replicad.CompoundBlueprint ||
+    shape instanceof replicad.Drawing
+  );
+};
+
+function createBlob(shellParameters) {
+  // note that you might want to do some caching for more complex models
+  return started.then(() => {
+    return generate3DPSD(shellParameters).blobSTL();
+  });
+}
+
+function createAssembly(
+  shellParameters,
+  updateModelProgress,
+  updateModel
+) {
+  return started
+    .then(() => {
+      return generate3DPSD(shellParameters, updateModelProgress);
+    })
+    .then((drumAssembly) => {
+      return drumAssembly
+        .filter(
+          ({ shape }) =>
+            !(shape instanceof replicad.Drawing) || shape.innerShape
+        )
+        .map(
+          ({
+            name,
+            shape,
+            color,
+            strokeType,
+            opacity,
+            highlight: inputHighlight,
+            highlightEdge,
+            highlightFace,
+          }) => {
+            let highlight =
+              inputHighlight ||
+              (highlightEdge && highlightEdge(new replicad.EdgeFinder())) ||
+              (highlightFace && highlightFace(new replicad.FaceFinder()));
+
+            const shapeInfo = { name, color, strokeType, opacity };
+
+            if (isBlueprintLike(shape)) {
+              shapeInfo.format = "svg";
+              shapeInfo.paths = shape.toSVGPaths();
+              shapeInfo.viewbox = shape.toSVGViewBox();
+              return shapeInfo;
+            }
+
+            try {
+              shapeInfo.mesh = shape.mesh({
+                tolerance: 0.1,
+                angularTolerance: 30,
+              });
+              shapeInfo.edges = shape.meshEdges({ keepMesh: true });
+            } catch (e) {
+              console.error(e);
+              shapeInfo.error = true;
+              return shapeInfo;
+            }
+
+            if (highlight)
+              try {
+                shapeInfo.highlight = highlight.find(shape).map((s) => {
+                  return s.hashCode;
+                });
+              } catch (e) {
+                console.error(e);
+              }
+
+            return shapeInfo;
+          }
+        );
+    })
+    .then((shapes) => updateModel(shapes));
+}
+
+expose({ createBlob, createAssembly });
+export default { createBlob, createAssembly}
