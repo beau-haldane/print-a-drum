48ab1dce27e641f11eff28b3bfa184d0986d007a
beau-haldane <beauhaldane@gmail.com>
2024-02-25 14:55:02 +1000

    add getShellConstants utility function

---
+++ src/model/constants.ts
@@ -0,0 +1,41 @@
+import { makePlane } from "replicad";
+import { ShellConstants } from "./types";
+import { calculateVertexAngle, inchesToMillimeters } from "./utils";
+
+export const getShellConstants = (
+  depthInches: number,
+  diameterInches: number,
+  lugNumber: number,
+  shellThickness: number,
+  fitmentTolerance: number
+): ShellConstants => {
+  const depth = inchesToMillimeters(depthInches);
+  const bearingEdgeHeight = 30;
+  const shellSegmentHeight = depth - bearingEdgeHeight * 2;
+  const radius = inchesToMillimeters(diameterInches) / 2;
+  const tabWidth = 10;
+  const tabThickness = Math.floor(shellThickness / 3);
+  const basePlane = makePlane("XY", 0);
+  const shellSegmentPlane = makePlane("XY", bearingEdgeHeight);
+  return {
+    bearingEdgeHeight,
+    bearingEdgeSegmentCoverage: 2,
+    depth,
+    radius,
+    shellSegmentVertexAngle: 360 / lugNumber,
+    shellSegmentHeight,
+    shellCenterPoint: bearingEdgeHeight + shellSegmentHeight / 2,
+    tabWidth,
+    tabVertexAngle: calculateVertexAngle(radius, tabWidth),
+    tabThickness,
+    tabOuterRadius: radius - (shellThickness - tabThickness) / 2,
+    tabFitmentToleranceDegrees: calculateVertexAngle(radius, fitmentTolerance),
+    interlockingTabHeight: 10,
+    diameterInches,
+    shellThickness,
+    fitmentTolerance,
+    lugNumber,
+    basePlane,
+    shellSegmentPlane,
+  };
+};
