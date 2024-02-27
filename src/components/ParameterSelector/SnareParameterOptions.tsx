04eff61ddd830d6b5a082d3fb484b4c63d31a640
beau-haldane <beauhaldane@gmail.com>
2024-02-27 11:24:35 +1000

    create SnareParameterOptions

---
+++ src/components/ParameterSelector/SnareParameterOptions.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ParameterAccordion } from "./ParameterAccordion";
+import { SelectInput, TextInput } from "./Input";
+
+export const SnareParameterOptions = ({
+  printableDrum,
+  register,
+  errors,
+  style,
+}) => {
+  return (
+    <ParameterAccordion title="Snare Bed Settings">
+      <div style={style}>
+        <TextInput
+          label="Snare Bed Angle"
+          initialValue={printableDrum.snareBeds.snareBedAngle}
+          register={register}
+          registerTo="snareBeds.snareBedAngle"
+          errors={errors?.snareBeds?.snareBedAngle}
+        />
+        <TextInput
+          label="Snare Bed Radius"
+          initialValue={printableDrum.snareBeds.snareBedRadius}
+          register={register}
+          registerTo="snareBeds.snareBedRadius"
+          errors={errors?.snareBeds?.snareBedRadius}
+        />
+        <TextInput
+          label="Snare Bed Depth"
+          initialValue={printableDrum.snareBeds.snareBedDepth}
+          register={register}
+          registerTo="snareBeds.snareBedDepth"
+          errors={errors?.snareBeds?.snareBedDepth}
+        />
+      </div>
+    </ParameterAccordion>
+  );
+};
