f3512ad0b2ee1c3f93c56fca788bf69126637eaa
beau-haldane <beauhaldane@gmail.com>
2024-02-29 00:26:12 +1000

    import .png so that Vite knows where to find it

--- src/replicad-studio-components/Material.tsx
+++ src/replicad-studio-components/Material.tsx
@@ -1,7 +1,9 @@
+// @ts-expect-error - Vite needs this to workaround declaring relative URLs
+import matcap from "/textures/matcap-1.png?url";
 import React from "react";
 import { useTexture } from "@react-three/drei";
 
 export default function Material(props) {
-  const [matcap1] = useTexture(["/textures/matcap-1.png"]);
+  const [matcap1] = useTexture([matcap]);
   return <meshMatcapMaterial color="#5a8296" matcap={matcap1} {...props} />;
 }
