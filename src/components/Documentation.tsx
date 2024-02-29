1839d7ca0e30608c58a6ac6784f836255353270d
beau-haldane <beauhaldane@gmail.com>
2024-02-29 11:24:56 +1000

    Add new documentation files to component

--- src/components/Documentation.tsx
+++ src/components/Documentation.tsx
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Markdown from "markdown-to-jsx";
 
-export const Documentation = ({style}) => {
+export const Documentation = ({ style }) => {
   const [shellSettings, setShellSettings] = useState("");
 
   useEffect(() => {
     (async () => {
       const shellSettings = await fetch("docs/ShellSettings.md");
       const lugSettings = await fetch("docs/LugSettings.md");
-
+      const bearingEdgeSettings = await fetch("docs/BearingEdgeSettings.md");
+      const snareBedSettings = await fetch("docs/SnareBedSettings.md");
       setShellSettings(
-        (await shellSettings.text()) + (await lugSettings.text())
+        (await shellSettings.text()) +
+          (await lugSettings.text()) +
+          (await bearingEdgeSettings.text()) +
+          (await snareBedSettings.text())
       );
     })();
   }, []);
 
-  return (
-    <Markdown
-      style={style}
-    >
-      {shellSettings}
-    </Markdown>
-  );
+  return <Markdown style={style}>{shellSettings}</Markdown>;
 };
